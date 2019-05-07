705e785859925438ae5cfc509376b0c0e6c8a1ce
Felix Parker <flixpar@gmail.com>
2019-05-07 18:28:56 -0400

    changed payoffs

--- game.py
+++ game.py
@@ -12,15 +12,15 @@ class GameState:
 			p1, p2 = self.player1.action(self.state, 0), self.player2.action(self.state, 1)
 			self.state.append((p1, p2))
 			if p1 == 0 and p2 == 0:
-				rewards[0] += -1
-				rewards[1] += -1
+				rewards[0] += 2
+				rewards[1] += 2
 			elif p1 == 0 and p2 == 1:
-				rewards[0] += -3
-				rewards[1] += 0
+				rewards[0] += -1
+				rewards[1] += 3
 			elif p1 == 1 and p2 == 0:
-				rewards[0] += 0
-				rewards[1] += -3
+				rewards[0] += 3
+				rewards[1] += -1
 			elif p1 == 1 and p2 == 1:
-				rewards[0] += -2
-				rewards[1] += -2
+				rewards[0] += 0
+				rewards[1] += 0
 		return rewards[0] / self.n_iter, rewards[1] / self.n_iter
